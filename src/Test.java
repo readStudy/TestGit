7c911d248f1ea3d7992c753fe8e5aa2e87fffd21
Mark <e4572351@gmail.com>
2018-01-09 13:32:56 +0800

    merge test

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,11 @@ public class Test {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+        
+        // test branch 1
+        
+        // test branch 2
+        
     }
 
 }
