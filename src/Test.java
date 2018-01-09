9551c3830ca37202ab2f77653c8c7cb54a3e4552
Mark <e4572351@gmail.com>
2018-01-09 13:32:56 +0800

    rebase 2

--- src/Test.java
+++ src/Test.java
@@ -7,12 +7,16 @@ public class Test {
         // test branch 1
         
         // test branch 2
+<<<<<<< HEAD
 
         // test branch 3
         
         // test branch 4
         
         // test
+=======
+        
+>>>>>>> 7c911d2... merge test
     }
 
 }
